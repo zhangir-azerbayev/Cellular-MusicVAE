407805a1cf0233f6e9a528910142c44eaab4fff1
Zhangir Azerbayev <zazerbayev@gmail.com>
2021-12-21 19:02:24 -0500

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# CPSC431-Final
-A journey through latent space 
+# A Journey Through Latent Space 
+A program that takes you on a continuous interpolation through the manifold of melodies. 
+
+Below are instructions for running the project. 
+
+### Prerequisites
+Requires a Unix system with `python3`, `pip3`, and `gcc` installed. 
+
+### Packages 
+Make sure you have the following packages installed 
+```
+libfluidsynth1 
+fluid-soundfont-gm 
+build-essential 
+libasound2-dev 
+libjack-dev
+```
+
+### Python environment
+Run the following command to install all necessary python libraries. 
+```
+pip3 install pyfluidsynth magenta
+```
+I have run into `gcc` issues when trying to install `magenta` in a conda environment. If you are getting `gcc` errors, try disabling conda. 
+
+In case you keep running into compatibility issues with the python script, I've provided sample outputs from my local runs (see final_proj.scd). 
+
+### Running the program
+Open `final_proj.scd` in the SuperCollider IDE. 
--- forward_pass.py
+++ forward_pass.py
@@ -3,10 +3,6 @@ import glob
 
 BASE_DIR = "gs://download.magenta.tensorflow.org/models/music_vae/colab2"
 
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-
 print('Importing libraries and defining some helper functions...')
 import json 
 import magenta.music as mm
@@ -19,23 +15,23 @@ from google.protobuf.json_format import MessageToJson
 
 
 tf.disable_v2_behavior()
-print('Environment setup done')
+print('Environment setup done...')
 
-print('loading vectors')
+print('loading cellular automaton...')
 reader = np.loadtxt(open("temp.csv", "rb"), delimiter=",")
 vecs=[]
 for row in reader: 
     vecs.append(row)
 
-print('loading model')
+print('loading model...')
 mel_2bar_config = configs.CONFIG_MAP['cat-mel_2bar_big']
 mel_2bar = TrainedModel(mel_2bar_config, batch_size=4, checkpoint_dir_or_path=BASE_DIR + '/checkpoints/mel_2bar_big.ckpt')
 
-print('doing forward pass')
+print('doing forward pass...')
 temperature=1.0
 mel_2_samples = mel_2bar.decode(vecs, length=32, temperature=temperature)
 
-print("saving midi")
+print("saving midi...")
 
 try:
     os.mkdir("midi")
@@ -44,6 +40,5 @@ except:
 
 for i, sample in enumerate(mel_2_samples): 
     mm.sequence_proto_to_midi_file(sample, "midi/{}.mid".format(i))
-
 
 print('DONE')
